57f7ccc4608dc2d5ceaa0c00f979985c0251aa44
TrainmonAnon <36116671+TrainmonAnon@users.noreply.github.com>
2019-05-14 10:50:09 -0700

    update readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a project from the node101 course where we serve the Top Spots project and data through Express while logging with Morgan.
+
+To run this project, first clone the repository, then cd into it in bash and type npm install. Finally start the program by typing "npm start".
--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
+const express = require('express');
+const logger = require('morgan');
 
+const data = require('./data.json');
 
 // create your express server below
-var app;
+var app = express();
 
 // add your routes and middleware below
+app.get('/', (req, res) => {
+    res.send('index');
+});
+
+app.get('/data', (req, res) => {
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(data));
+});
+
+app.use(logger('dev'));
 
 // finally export the express application
 module.exports = app;
